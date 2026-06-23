40854ba8394c4e2ad02d9b19e7a4d944f5e7eec1
Eric <ericcidlopez0@gmail.com>
2026-06-23 19:08:54 +0200

    first commit

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 const Database = require('better-sqlite3');
-const db = new Database('dental_bot.db');
+const db = new Database('medical_bot.db');
 const { encrypt, decrypt } = require('./services/crypto');
 
 db.exec(`
